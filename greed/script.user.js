dfe4a3faf4f6275139f524b3fde046d541b0044c
ling1729 <renlingfeng21@gmail.com>
2020-03-18 21:21:06 -0700

    numbers

--- greed/script.user.js
+++ greed/script.user.js
@@ -9,18 +9,19 @@
 // @grant GM.setValue
 // ==/UserScript==
 var day;
-
+var min = 10;
+var max =  50;
 (async function() {
     'use strict';
     day = new Date().getDay();
     await GM.setValue("date", day);
     console.log(await GM.getValue("date"));
-    let randomNumber = Math.floor(Math.random() * Math.floor(20) + 30);
+    let randomNumber = Math.floor(Math.random() * (max - min + 1) + min);
     document.getElementById('input-number').value = randomNumber;
     document.getElementById('submit-button').click()
 })();
 
-setInterval(checkDay,1000);
+setInterval(checkDay,10000);
 
 async function checkDay(){
     if(new Date().getDay() != await GM.getValue("date")){

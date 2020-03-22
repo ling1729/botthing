494179f8428e6344a021629afd4f34d231a31caa
ling1729 <renlingfeng21@gmail.com>
2020-03-22 10:25:01 -0700

    steal

--- reap/script.user.js
+++ reap/script.user.js
@@ -13,6 +13,81 @@ var random = Math.random() * (max - min) + min;
 	'use strict';
 	console.log(getSeconds());
 })();
+function parseTime(e) {
+    var t = e.split(",");
+    var n = 0;
+    for (var r in t) {
+        var i = t[r].trim();
+        var s = parseInt(i);
+        if (i.indexOf("ay") != -1) {
+            n += s * 60 * 60 * 24
+        } else if (i.indexOf("our") != -1) {
+            n += s * 60 * 60
+        } else if (i.indexOf("inute") != -1) {
+            n += s * 60
+        } else if (i.indexOf("econd") != -1) {
+            n += s
+        }
+    }
+    return n
+}
+
+function unParseTime(e, t) {
+    var n = "";
+    e = Math.round(e);
+    if (e < 0)
+        e = 0;
+    if (typeof t == "undefined") {
+        var t = true
+    }
+    var r = Math.floor(e / (60 * 60 * 24));
+    e -= r * 60 * 60 * 24;
+    var i = Math.floor(e / (60 * 60));
+    e -= i * 60 * 60;
+    var s = Math.floor(e / 60);
+    e -= s * 60;
+    if (r > 0) {
+        n = r + " day" + (r != 1 ? "s" : "");
+        if (i == 0 && s == 0 && e == 0) {
+            return n
+        }
+        n += ", ";
+        n += i + " hour" + (i != 1 ? "s" : "");
+        if (s == 0 && e == 0) {
+            return n
+        }
+        n += ", ";
+        n += s + " minute" + (s != 1 ? "s" : "");
+        if (e == 0 || !t) {
+            return n
+        }
+        n += ", " + e + " second" + (e != 1 ? "s" : "");
+        return n
+    }
+    if (i > 0) {
+        n = i + " hour" + (i != 1 ? "s" : "");
+        if (s == 0 && e == 0) {
+            return n
+        }
+        n += ", ";
+        n += s + " minute" + (s != 1 ? "s" : "");
+        if (e == 0 || !t) {
+            return n
+        }
+        n += ", " + e + " second" + (e != 1 ? "s" : "");
+        return n
+    }
+    if (s > 0) {
+        n = s + " minute" + (s != 1 ? "s" : "");
+        if (e == 0 || !t) {
+            return n
+        }
+        n += ", " + e + " second" + (e != 1 ? "s" : "");
+        return n
+    }
+    return e + " second" + (e != 1 ? "s" : "")
+}
+/*
 function parseTime(x){
 	let times = [0, 0, 0];
 	let seconds = 0;
@@ -44,7 +119,7 @@ function parseTime(x){
 	for(let i = 0; i < times.length; i ++)
 	seconds += Math.pow(60, times.length - 1 - i) * times[i];
 	return seconds;
-}
+}*/
 function getSeconds(){
 	return parseTime(document.getElementById('last-reap').innerHTML);
 }
@@ -76,7 +151,7 @@ function getRecent(){
 }
 function beatReaper(){
 	let average = getRecent();
-	document.getElementById('current-game').childNodes[3].innerHTML = "target: " + average.toFixed(2) + " seconds";
+	document.getElementById('current-game').childNodes[3].innerHTML = "target: " + unParseTime(average);
 	if(document.getElementById('reap-button-container').style.display !== 'none' && getSeconds() >= random + average){
 		document.getElementById('reap-button').click();
 		random = Math.random() * (max - min) + min;
